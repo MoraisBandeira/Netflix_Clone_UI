0eacf0c0e8033a8b1ebc60dd16b2d4888e773ad2
MoraisBandeira <anderson.moraisbandeira@gmail.com>
2020-10-09 15:25:58 -0300

    Updates

---
+++ precache-manifest.3980cf3c986ee3970f5a2f426ba826ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91dd33ff6ada22edeae83f61ffb2c92",
+    "url": "/Netflix_Clone_UI/index.html"
+  },
+  {
+    "revision": "3289a2a5fb5f15f24244",
+    "url": "/Netflix_Clone_UI/static/css/main.1e468021.chunk.css"
+  },
+  {
+    "revision": "c6c57c42b36c51acd5d2",
+    "url": "/Netflix_Clone_UI/static/js/2.70d06f39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Netflix_Clone_UI/static/js/2.70d06f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3289a2a5fb5f15f24244",
+    "url": "/Netflix_Clone_UI/static/js/main.2942c74b.chunk.js"
+  },
+  {
+    "revision": "7169d7aa1befb39ac5ae",
+    "url": "/Netflix_Clone_UI/static/js/runtime-main.a630a464.js"
+  }
+]);
